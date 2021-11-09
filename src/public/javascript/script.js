315dea67205636a71ad666b65aa91fb1deba0172
Khoa <khoa.ha@zodinet.tech>
2021-11-09 13:32:20 +0700

    done

--- src/public/javascript/script.js
+++ src/public/javascript/script.js
@@ -1,106 +1,263 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-const billElementGlobal = document.querySelector('input[name=bill]');
-const tipCustomElementGlobal =  document.querySelector('input[name=tipPercentCustom]');
+// bill
+const billElementGlobal = $('input[name=bill]');
+const billErrorElement = $('#bill-error');
 
-const personCountElementGlobal = document.querySelector('input[name=personCount]');
+const tipElementsGlobal =  $$('input[name=tipPercent]');
 
-billElementGlobal.addEventListener('blur', (event) => {
-  checkPrice();
-}, true);
+// tip custom
+const tipCustomBoxElementGlobal =  $('#custom-tip');
+const tipCustomElementGlobal =  $('input[name=tipPercentCustom]');
 
-tipCustomElementGlobal.addEventListener('blur', (event) => {
-  checkPrice();
-}, true);
+// person count
+const personCountElementGlobal = $('input[name=personCount]');
+const personCountErrorElement = $('#person-count-error');
 
-personCountElementGlobal.addEventListener('blur', (event) => {
-  checkPrice();
-}, true);
+// result
+const amountResultElementGlobal = $('#amount-result');
+const totalResultElementGlobal = $('#total-result');
 
-function handleReset(){
-  billElementGlobal.value = '';
-  let tipElements =  document.getElementsByName('tipPercent');
+// reset button
+const resetBtnElement = $('#reset-button');
 
-  for(let i = 0; i < tipElements.length; i++) {
-    if(tipElements[i].checked)
-      tipElements[i].checked = false;
-  }
-  personCountElementGlobal.value = '';
+const app = {
+  bill: 0,
+  tipNumber: 0,
+  tipNumberCustom: '',
+  percentCount: 0,
+  amountResult: '00.00',
+  totalResult: '00.00',
+  isResetDisabled: false,
 
-  let amountResultElement = document.getElementById('amount-result');
-  let totalResultElement = document.getElementById('total-result');
+  onTipPercentClick: function(){
+    this.tipNumber = '';
+    tipCustomElementGlobal.value = '';
+    tipCustomBoxElementGlobal.classList.remove("onCustom");
+  },
 
-  amountResultElement.innerHTML = '';
-  totalResultElement.innerHTML = '';
+  isCustomTip: function(){
+    console.log('custom tip');
+    tipCustomBoxElementGlobal.classList.add("onCustom");
+  },
 
-  const customTip = document.getElementById('custom-tip');
-  customTip.classList.remove("onCustom");
-}
+  handleReset: function(){
+    // bill remove
+    billElementGlobal.value = 0;
 
-function onTipPercentClick(){
-  checkPrice();
-
-}
-
-function isCustomTip (){
-  const customTip = document.getElementById('custom-tip');
-  customTip.classList.add("onCustom");
-}
-
-function checkPrice(){
-
-  // handle bill element
-  let billElement = document.querySelector('input[name=bill]');
-  let billErrorElement = document.getElementById('bill-error');
-
-  if(billElement.value ===''){
-    billErrorElement.innerHTML= 'Bill must be add';
-    return;
-  }
-  document.getElementById('bill-error').innerHTML= '';
-
-  // handle tip percent element
-  let tipElements =  document.getElementsByName('tipPercent');
-  let tipCustomElement =  document.querySelector('input[name=tipPercentCustom]');
-
-  let tipNumber = 0;
-
-  console.log(tipCustomElement.value);
-  if(tipCustomElement.value===''){
-    console.log(tipElements);
-
-    for(let i = 0; i < tipElements.length; i++) {
-      if(tipElements[i].checked)
-        tipNumber= parseFloat(tipElements[i].value);
+    // tip remove
+    for(let i = 0; i < tipElementsGlobal.length; i++) {
+      if(tipElementsGlobal[i].checked)
+        tipElementsGlobal[i].checked = false;
     }
-  }else {
-    tipNumber=tipCustomElement.value;
-  }
 
-  // handle count person element
+    // tip custom remove
+    tipCustomElementGlobal.classList.remove("onCustom");
+    tipCustomElementGlobal.value= '';
 
-  let personCountElement = document.querySelector('input[name=personCount]');
-  let personCountErrorElement = document.getElementById('person-count-error');
+    // person count remove
+    personCountElementGlobal.value = 0;
 
-  if(personCountElement.value ===''){
-    personCountErrorElement.innerHTML= 'Number of people must be add';
-    return;
-  }
-  document.getElementById('person-count-error').innerHTML= '';
+    //result remove
+    amountResultElementGlobal.innerHTML = '00.00';
+    totalResultElementGlobal.innerHTML = '00.00';
 
+    // disable reset button
+    this.isResetDisabled = true;
+    resetBtnElement.disabled = true;
+    resetBtnElement.classList.add('btn-disabled');
+  },
 
-  let amountResultElement = document.getElementById('amount-result');
-  let totalResultElement = document.getElementById('total-result');
+  handleResetAvailable: function (){
+    this.bill = parseFloat(billElementGlobal.value);
+    this.percentCount = parseFloat(personCountElementGlobal.value);
 
-  let amountResult = (parseFloat(billElement.value) * (tipNumber) / 100) / 5;
+    if (this.bill === 0 && this.percentCount === 0){
+      this.isResetDisabled = true;
+      resetBtnElement.disabled = true;
+      resetBtnElement.classList.add('btn-disabled');
+    }else {
+      this.isResetDisabled = false;
+      resetBtnElement.disabled = false;
+      resetBtnElement.classList.remove('btn-disabled');
+    }
+  },
 
-  console.log(billElement.value, tipNumber, personCountElement.value, 'main');
+  onsubmit: function (){
+    console.log('onsubmit');
 
-  let totalResult = (
-    (parseFloat(billElement.value) * tipNumber / 100)
-    + parseFloat(billElement.value)
-  )/5 ;
+    // handle bill element
+    if(billElementGlobal.value ===''){
+      billErrorElement.innerHTML= 'Bill must be add';
+      return;
+    }
+    if(billElementGlobal.value<=0){
+      billErrorElement.innerHTML= 'Bill must be greater than';
+      return;
+    }
+    this.bill = parseFloat(billElementGlobal.value);
+    billErrorElement.innerHTML= '';
 
-  amountResultElement.innerHTML = amountResult.toFixed(2).toString();
-  totalResultElement.innerHTML = totalResult.toFixed(2).toString();
+    // handle tip percent element
 
+    console.log(tipCustomElementGlobal.value);
+    if(tipCustomElementGlobal.value=== ''){
+      console.log(tipElementsGlobal);
+
+      for(let i = 0; i < tipElementsGlobal.length; i++) {
+        if(tipElementsGlobal[i].checked)
+          this.tipNumber= parseFloat(tipElementsGlobal[i].value);
+      }
+    }else {
+      this.tipNumber= parseFloat(tipCustomElementGlobal.value);
+    }
+
+    // handle count person element
+
+    if(personCountElementGlobal.value === '0'){
+      personCountErrorElement.innerHTML= 'Number of people must be add';
+      return;
+    }
+    if(personCountElementGlobal.value <= 0){
+      personCountErrorElement.innerHTML= 'Number of people must be greater than 0';
+      return;
+    }
+
+    this.percentCount = parseFloat(personCountElementGlobal.value);
+    personCountErrorElement.innerHTML= '';
+
+    amountResultElementGlobal.innerHTML = '--.--';
+    totalResultElementGlobal.innerHTML = '--.--';
+    const url = 'https://plitter-server.vercel.app/api/calculate?' +
+      `bill=${this.bill}&people=${this.percentCount}&tipPercent=${this.tipNumber}`
+
+    console.log(url);
+    this.getData(
+      url
+    ).then(res=>{
+      console.log(res);
+      if(res['result']===true){
+        this.amountResult = res['amount'];
+        this.totalResult = res['total'];
+        amountResultElementGlobal.innerHTML = this.amountResult.toFixed(2).toString();
+        totalResultElementGlobal.innerHTML = this.totalResult.toFixed(2).toString();
+      }
+    })
+  },
+
+  getData: async function (url = '') {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        // 'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade,
+      // origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      // params: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  },
+
+  postData: async function (url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin,
+      // same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  },
+
+  checkPrice: function (){
+    // console.log('out blur');
+  },
+
+  // checkPrice :function (){
+  //
+  //   // handle bill element
+  //   if(billElementGlobal.value ===''){
+  //     billErrorElement.innerHTML= 'Bill must be add';
+  //     return;
+  //   }
+  //   if(billElementGlobal.value<=0){
+  //     billErrorElement.innerHTML= 'Bill must be greater than';
+  //     return;
+  //   }
+  //   this.bill = parseFloat(billElementGlobal.value);
+  //   billErrorElement.innerHTML= '';
+  //
+  //   // handle tip percent element
+  //
+  //   console.log(tipCustomElementGlobal.value);
+  //   if(tipCustomElementGlobal.value=== ''){
+  //     console.log(tipElementsGlobal);
+  //
+  //     for(let i = 0; i < tipElementsGlobal.length; i++) {
+  //       if(tipElementsGlobal[i].checked)
+  //         this.tipNumber= parseFloat(tipElementsGlobal[i].value);
+  //     }
+  //   }else {
+  //     this.tipNumber= parseFloat(tipCustomElementGlobal.value);
+  //   }
+  //
+  //   // handle count person element
+  //
+  //   if(personCountElementGlobal.value === '0'){
+  //     personCountErrorElement.innerHTML= 'Number of people must be add';
+  //     return;
+  //   }
+  //   if(personCountElementGlobal.value <= 0){
+  //     personCountErrorElement.innerHTML= 'Number of people must be greater than 0';
+  //     return;
+  //   }
+  //
+  //   this.percentCount = parseFloat(personCountElementGlobal.value);
+  //   personCountErrorElement.innerHTML= '';
+  //
+  //   // calculate result
+  //   this.amountResult = (
+  //     this.bill * this.tipNumber / 100
+  //   ) / this.percentCount;
+  //   this.totalResult = (
+  //     (this.bill * this.tipNumber / 100) + this.bill
+  //   ) / this.percentCount;
+  //
+  //   // show result
+  //   amountResultElementGlobal.innerHTML = this.amountResult.toFixed(2).toString();
+  //   totalResultElementGlobal.innerHTML = this.totalResult.toFixed(2).toString();
+  //
+  // },
+
+  start: function () {
+    billElementGlobal.addEventListener('blur', (event) => {
+      this.checkPrice();
+    }, true);
+    tipCustomElementGlobal.addEventListener('blur', (event) => {
+      this.checkPrice();
+    }, true);
+    personCountElementGlobal.addEventListener('blur', (event) => {
+      this.checkPrice();
+    }, true);
+    billElementGlobal.addEventListener('change', (event) => {
+      this.handleResetAvailable();
+    });
+  },
 }
+
+app.start();
